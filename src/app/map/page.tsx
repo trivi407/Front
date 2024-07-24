83ff00be8390aef9de52bfb563146781000c8d29
Nam Eungyeong <nek1717@naver.com>
2024-07-24 19:47:22 +0900

    feat: 구글맵 로드 방식 수정

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,46 +1,60 @@
 "use client";
 import { useEffect, useCallback, useRef, useState } from "react";
 
-export default function My() {
+export default function Map() {
   const mapRef = useRef<HTMLDivElement>(null);
   const [map, setMap] = useState(null);
 
   const initMap = useCallback(() => {
     if (window.google && !map) {
-      navigator.geolocation.getCurrentPosition(function (pos) {
-        const curLocation = {
-          lat: pos.coords.latitude,
-          lng: pos.coords.longitude,
-        };
+      const curLocation = {
+        lat: 37.5665,
+        lng: 126.978,
+      };
 
-        const map = new window.google.maps.Map(mapRef.current, {
-          center: curLocation,
-          zoom: 13,
-        });
+      // navigator.geolocation.getCurrentPosition(function (pos) {
+      //   const curLocation = {
+      //     lat: pos.coords.latitude,
+      //     lng: pos.coords.longitude,
+      //   };
+      // });
 
-        setMap(map);
+      const map = new window.google.maps.Map(mapRef.current, {
+        center: curLocation,
+        zoom: 13,
       });
+
+      setMap(map);
     }
-  }, []);
+  }, [map]);
 
   useEffect(() => {
-    window.initMap = initMap;
+    const scriptId = "google-maps-script";
 
-    if (typeof window !== undefined) {
-      if (!window.google) {
-        const script = document.createElement("script");
-        script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&callback=initMap`;
-        script.defer = true;
-        document.head.appendChild(script);
-      } else {
-        initMap();
-      }
+    if (!document.getElementById(scriptId)) {
+      const script = document.createElement("script");
+      script.id = scriptId;
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&callback=initMap`;
+      script.defer = true;
+      script.async = true;
+      script.onerror = () => {
+        console.error("Error: loading Google Maps script");
+      };
+
+      document.head.appendChild(script);
+
+      script.onload = () => {
+        if (window.google) initMap();
+      };
+    } else {
+      if (window.google) initMap();
     }
 
+    window.initMap = initMap;
     return () => {
       window.initMap = null;
     };
-  }, []);
+  }, [initMap]);
 
   return (
     <div
